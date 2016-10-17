3d906898dfcd1abc66ad450c818f1d05b0063ff7
grape <oneheed@gmail.com>
2016-10-18 00:37:18 +0800

    Create Veiw, Model, Contrllers

---
+++ MVC5/Controllers/CustomDataController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5.Models;
+
+namespace MVC5.Controllers
+{
+    public class CustomDataController : Controller
+    {
+        private CustomEntities db = new CustomEntities();
+
+        // GET: CustomData
+        public ActionResult Index()
+        {
+            return View(db.客戶資料.ToList());
+        }
+
+        // GET: CustomData/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // GET: CustomData/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomData/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶資料.Add(客戶資料);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(客戶資料);
+        }
+
+        // GET: CustomData/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: CustomData/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶資料).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(客戶資料);
+        }
+
+        // GET: CustomData/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            if (客戶資料 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶資料);
+        }
+
+        // POST: CustomData/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶資料 客戶資料 = db.客戶資料.Find(id);
+            db.客戶資料.Remove(客戶資料);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
